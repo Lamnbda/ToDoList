5dd7de679c02725b1573ad2e9cea12c5ad3d52cb
VincentLabor <vlabor95@gmail.com>
2019-05-16 00:32:46 -0700

    User can send requests to server

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,45 +3,34 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
     var today = new Date();
     var currentDay = today.getDay();
-    var day = "";
+ 
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-            default: 
-            console.log("Current day is equal to " + currentDay);
-    }
+var options ={
+    weekday: 'long', 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric'
+};
+
+var day = today.toLocaleDateString('en-US', options);
 
     res.render('list', {
         kindOfDay: day
     })
+
+    
 });
+
+app.post("/", function(req,res){
+console.log(req.body.toDoList);
+})
 
 
 app.listen(3000, function () {
