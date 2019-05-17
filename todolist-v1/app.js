7288626e6fc92ada4b79f73c0d2be7aea84fe1db
VincentLabor <vlabor95@gmail.com>
2019-05-17 12:43:44 -0700

    CSS added

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,24 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
-var items = ["Make food", "Eat food", "Clean dishes"];
+let items = ["Make food", "Eat food", "Clean dishes"];
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function (req, res) {
-    var today = new Date();
+    let today = new Date();
 
-var options ={
+let options ={
     weekday: 'long', 
     year: 'numeric', 
     month: 'long', 
     day: 'numeric'
 };
 
-var day = today.toLocaleDateString('en-US', options);
+let day = today.toLocaleDateString('en-US', options);
 
     res.render('list', {kindOfDay: day,newListItem: items})
 
@@ -27,7 +28,7 @@ var day = today.toLocaleDateString('en-US', options);
 });
 
 app.post("/", function(req,res){
-    var lists = req.body.toDoList;
+    let lists = req.body.toDoList;
     items.push(lists);
 
     res.redirect('/');
