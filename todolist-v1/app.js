f2a25d3d8adda23c67e25cbf3d7be67b7ac96dbb
VincentLabor <vlabor95@gmail.com>
2019-05-17 16:04:05 -0700

    Transfer or data to proper pages

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -3,9 +3,12 @@ const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
 let items = ["Make food", "Eat food", "Clean dishes"];
+let workItems = [];
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
@@ -13,25 +16,46 @@ app.set('view engine', 'ejs');
 app.get("/", function (req, res) {
     let today = new Date();
 
-let options ={
-    weekday: 'long', 
-    year: 'numeric', 
-    month: 'long', 
-    day: 'numeric'
-};
+    let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
 
-let day = today.toLocaleDateString('en-US', options);
-
-    res.render('list', {kindOfDay: day,newListItem: items})
-
-    
+    let day = today.toLocaleDateString('en-US', options);
+    res.render('list', {
+        listTitle: day,
+        newListItem: items
+    })
 });
 
-app.post("/", function(req,res){
+app.post("/", function (req, res) {
     let lists = req.body.toDoList;
-    items.push(lists);
 
-    res.redirect('/');
+    console.log(req.body);
+
+    if (req.body.list === 'Work') {
+        workItems.push(lists);
+        res.redirect('/work')
+    } else {
+        items.push(lists);
+        res.redirect('/');
+    }
+
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItem: workItems
+    });
+})
+
+app.post("/work", function (req, res) {
+    let lists = req.body.toDoList;
+    workItems.push(lists);
+    res.redirect("/work")
 })
 
 
